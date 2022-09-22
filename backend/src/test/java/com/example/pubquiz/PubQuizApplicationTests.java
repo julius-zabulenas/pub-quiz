45577ecbae5b4e4373064d0dcb7e5f99748c8ff1
Julius <jzabulenas@protonmail.com>
2022-09-22 15:57:18 +0300

    add backend folder, fill it with starter Spring Boot project

---
+++ backend/src/test/java/com/example/pubquiz/PubQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pubquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PubQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
